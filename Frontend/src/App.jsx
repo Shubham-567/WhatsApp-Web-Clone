ac45c506a93e91f0be320d1f973ec63abae5d9a1
Shubham <shubhampatil1356@gmail.com>
2025-08-07 21:34:53 +0530

    Frontend: Set up axios and zustand for chats and messages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import API from "./services/api";
+
+const fetchChats = async () => {
+  const res = await API.get("/conversations");
+  console.log(res.data);
+};
+
 function App() {
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
   return (
-    <div className='text-center text-5xl font-bold mt-20 text-orange-500'>
+    <div className='text-center text-5xl font-bold mt-20 text-txt-secondary'>
       Testing..
     </div>
   );
---
+++ Frontend/src/store/chatStore.js
@@ -0,0 +1,54 @@
+import { create } from "zustand";
+import API from "../services/api.js";
+import { sendMessage } from "../../../Backend/controllers/messageController";
+
+const useChatStore = create((set) => ({
+  chats: [],
+  messages: [],
+  activeChat: null,
+  loading: false,
+
+  fetchChats: async () => {
+    set({ loading: true });
+
+    try {
+      const res = await API.get("/conversations");
+      set({ chats: res.data, loading: false });
+    } catch (error) {
+      console.error("Failed to fetch chats ", error.message);
+      set({ loading: false });
+    }
+  },
+
+  fetchMessages: async (wa_id) => {
+    set({ loading: true });
+
+    try {
+      const res = await API.get(`/messages/${wa_id}`);
+      set({ messages: res.data, activeChat: wa_id, loading: false });
+    } catch (error) {
+      console.error("Failed to fetch messages ", error.message);
+      set({ loading: false });
+    }
+  },
+
+  sendMessage: async (wa_id, text) => {
+    if (!text.trim()) {
+      return;
+    }
+
+    try {
+      const res = await API.post(`/messages/${wa_id}/send`, {
+        text,
+        name: "You",
+      });
+
+      // assuming success and update ui instantly
+      set((state) => ({
+        messages: [...state.message, res.data],
+      }));
+    } catch (error) {
+      console.error("Failed to send message", error.message);
+    }
+  },
+}));
